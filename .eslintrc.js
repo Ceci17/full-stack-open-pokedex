05543d7e4e7162ff9daaa4178c5bdc5d022788e3
Stefan Nikolic <stefan.nikolic@united.cloud>
2021-03-20 20:43:43 +0100

    set cypress globals true

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     es6: true,
     node: true,
     "jest/globals": true,
+    "cypress/globals": true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
@@ -26,9 +27,4 @@ module.exports = {
     "no-console": "error",
     "react/prop-types": 0,
   },
-  {
-    "globals": {
-      "cy": true
-    }
-  }
 };
